409fa91b2c45928452539e909e146c230e357a5c
Spook <sajjad_spook@yahoo.com>
2020-04-04 19:13:16 +0430

    init

---
+++ SpotPay.Presentation/Controllers/WeatherForecastController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace SpotPay.Presentation.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            JsonData data = new JsonData
+            {
+                key = "1",
+                one = "Ali"
+            };
+            JsonResult json = new JsonResult(data)
+            {
+                Value = data,
+            };
+            return json;
+        }
+
+        private class JsonData
+        {
+           public string one { get; set; }
+            public string key { get; set; }
+        }
+    }
+}
